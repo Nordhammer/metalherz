b7bfe37ac85238370f291016fdd8959e17f68380
nordhammer <torsten.nicodemus@hotmail.de>
2018-11-25 21:04:34 +0100

    meta theme color hinzugefügt

--- template/layout/header.blade.php
+++ template/layout/header.blade.php
@@ -2,6 +2,7 @@
 <html lang="de">
     <head>
         <meta charset="utf-8">
+        <meta name="theme-color" content="rgb(0,0,0)">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="keywords" content="{KEYWORDS}">
         <meta name="description" content="{DESCRIPTION}">
